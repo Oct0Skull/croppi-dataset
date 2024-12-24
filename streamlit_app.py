ca1fdea388363f15aaaf1b00fdfd05135080544a
Octo_Skull <17036615+Oct0Skull@users.noreply.github.com>
2024-12-24 20:34:01 +0000

    correr prueba 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,118 @@
-import altair as alt
-import pandas as pd
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
-st.write(
-    """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
-    """
+st.title('Elementos de interfaz :iphone:')
+
+###
+# Static Table
+###
+st.header('Static Table')
+dataframe = pd.DataFrame(
+    np.random.randn(10,20),
+    columns = ('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+
+###
+# Dinamuc DF
+###
+st.header('Dynamic Data Frame')
+dataframe = pd.DataFrame(
+    np.random.randn(10,20),
+    columns = ('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0)) # 👈 resalta el valor máz de cada columna
+
+
+###
+# Chart
+###
+st.header('Line Chart')
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns = ['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+###
+# Map
+###
+st.header('Map')
+#19.432608, -99.133209.
+map_data = pd.DataFrame(
+    np.random.randn(250,2) / [50,50] + [19.43,-99.13],
+    columns = ['lat', 'lon'])
+
+st.map(map_data)
+
+
+###
+# Slider
+###
+st.header('Slider')
+x= st.slider('x') # 👈 this is a widget
+st.write(x,'squared is ', x*x)
+
+
+###
+# Text input
+###
+st.header('Text Input')
+st.text_input("Your name", key="name")
+
+st.session_state.name 
+
+
+###
+# Check Box
+###
+st.header('Check Box')
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+        np.random.randn(20,3),
+        columns = ['a', 'b', 'c'])
+    
+    chart_data
+
+
+###
+# Columns
+###
+st.header('Columns')
+left_column, right_column = st.columns(2)
+
+# Left column
+left_column.subheader('Left column') # 👈 se aplica la funcion sobre el nombre del objeto de posicion que creamos
+left_column.button('Press me!')      # objeto de posición: left_column 
+
+# Rigth column using with
+with right_column:
+    st.subheader('Right column')
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Raivenclaw", "Hufflepuff", "slytherin"))
+    st.write(f'You are in {chosen} house! ')
+
+
+###
+# Slidebar items
+###
+
+# Selectbox
+st.sidebar.subheader('Selectbox')       # 👈 Barra lateral oculta/muestra
+add_selectbox = st.sidebar.selectbox(
+    'How do you like to be contacted',
+    ('Email', 'Home phone','Mobile phone')
+)
+
+# Slider
+st.sidebar.subheader('Slider')
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0) 
 )
 
 
-# Load the data from a CSV. We're caching this so it doesn't reload every time the app
-# reruns (e.g. if the user interacts with the widgets).
-@st.cache_data
-def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
-    return df
-
-
-df = load_data()
-
-# Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-)
-
-# Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
-
-# Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-)
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-
-
-# Display the data as a table using `st.dataframe`.
-st.dataframe(
-    df_reshaped,
-    use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
-)
-
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
-    )
-    .properties(height=320)
-)
-st.altair_chart(chart, use_container_width=True)
